59d9608d617ef61ef0294b554acc61c0498b9330
Kevin Bett <prodigy4614@gmail.com>
2024-11-01 13:27:49 +0300

    rendered HogTile component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import HogTile from "./HogTile";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<div className="hog-container">
+            {hogs.map((hog) => (
+                <HogTile key={hog.name} name={hog.name} image={hog.image} />
+            ))}
+        </div>
 		</div>
 	);
 }
